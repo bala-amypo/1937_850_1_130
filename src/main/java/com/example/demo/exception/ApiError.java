58b0700aefad3b207923d33850bb5fec9e475d79
bala-amypo <bala@amypo.in>
2025-12-21 14:03:32 +0000

    User commit

--- src/main/java/com/example/demo/exception/ApiError.java
+++ src/main/java/com/example/demo/exception/ApiError.java
@@ -2,4 +2,13 @@ package com.example.demo.exception;
 
 public class ApiError {
 
+    private String message;
+
+    public ApiError(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,5 +1,28 @@
 package com.example.demo.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiError> handleIllegalArgument(IllegalArgumentException ex) {
+        return new ResponseEntity<>(
+                new ApiError(ex.getMessage()),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ApiError> handleNoSuchElement(NoSuchElementException ex) {
+        return new ResponseEntity<>(
+                new ApiError(ex.getMessage()),
+                HttpStatus.NOT_FOUND
+        );
+    }
 }
