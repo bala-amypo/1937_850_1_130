7961c9af0fe0cc588baaf66c975394fce35383ea
bala-amypo <bala@amypo.in>
2025-12-23 04:58:23 +0000

    User commit

--- src/main/java/com/example/demo/model/StolenDeviceReport.java
+++ src/main/java/com/example/demo/model/StolenDeviceReport.java
@@ -1,17 +1,22 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
+import java.time.LocalDateTime;
 
-@Data
 @Entity
 @Table(name = "stolen_device_reports")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class StolenDeviceReport {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String serialNumber;
+
     private String reportReason;
+
+    private LocalDateTime reportedAt;
 }
