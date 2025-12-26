fc6bb27262c59a6bd31d595c52c7f86baefd47cb
bala-amypo <bala@amypo.in>
2025-12-26 08:27:04 +0000

    User commit

--- src/main/java/com/example/demo/controller/FraudRuleController.java
+++ src/main/java/com/example/demo/controller/FraudRuleController.java
@@ -1,12 +1,34 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.FraudRule;
 import com.example.demo.service.impl.FraudRuleServiceImpl;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/rules")
 public class FraudRuleController {
 
-    private final FraudRuleServiceImpl service;
+    private final FraudRuleServiceImpl ruleService;
 
-    public FraudRuleController(FraudRuleServiceImpl service) {
-        this.service = service;
+    public FraudRuleController(FraudRuleServiceImpl ruleService) {
+        this.ruleService = ruleService;
+    }
+
+    @PostMapping("/create")
+    public FraudRule createRule(@RequestBody FraudRule rule) {
+        return ruleService.createRule(rule);
+    }
+
+    @GetMapping("/active")
+    public List<FraudRule> getActiveRules() {
+        return ruleService.getActiveRules();
+    }
+
+    @GetMapping("/{ruleCode}")
+    public Optional<FraudRule> getRuleByCode(@PathVariable String ruleCode) {
+        return ruleService.getRuleByCode(ruleCode);
     }
 }
